5eda3b65051642ef94a2ede26fdeddc8166ac341
Charlie Robson <crobson29@gmail.com>
2024-06-05 16:54:30 -0500

    Created Notification Manager
    Sends an email when given a message.

--- notification_manager.py
+++ notification_manager.py
@@ -1,3 +1,26 @@
+import os
+from dotenv import load_dotenv
+import smtplib
+
+load_dotenv()
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+    def __init__(self) -> None:
+        self._email = os.environ["My_Email"]
+        self._password = os.environ["Email_Password"]
+        
+    def send_message(self, message):
+        '''
+        Sends an email when the price of a flight dips below the lowest price.
+        
+        Parameters:
+        message (str): the body of the email
+        
+        Returns:
+        None
+        '''
+        
+        with smtplib.SMTP('smtp.gmail.com') as connection:
+            connection.starttls()
+            connection.login(user=self._email, password=self._password)
+            connection.sendmail(from_addr=self._email, to_addrs=os.environ["To_Email"], msg=message)

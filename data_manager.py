ebc5e5b254d36f56ff4c62a642b86fbaa36bcd42
Charlie Robson <crobson29@gmail.com>
2024-06-05 14:39:21 -0500

    Created DataManager

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,34 @@
+import os
+import requests
+from dotenv import load_dotenv
+from requests.auth import HTTPBasicAuth
+
+load_dotenv()
+
+SHEETY_END = os.environ["Sheety_GET"]
+
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        self._user = os.environ["Sheety_User"]
+        self._password = os.environ["Sheety_password"]
+        self._authorization = HTTPBasicAuth(self._user, self._password)
+        self.destination_data = {}
+        
+    def get_destination_data(self):
+        response = requests.get(url=SHEETY_GET_END)
+        data = response.json()
+        
+        self.destination_data = data["Flights"]
+        
+        return self.destination_data
+    
+    
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(url=f"{SHEETY_END}/{city["id"]}", json=new_data)
